b9ff637a278d49fc707296280a2761f4c786fe31
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-28 23:20:42 +0300

    Update ProductController.cs
    We use the Linq to query the database;

--- SportsStore2/SportsStore2/Controllers/ProductController.cs
+++ SportsStore2/SportsStore2/Controllers/ProductController.cs
@@ -10,16 +10,16 @@ namespace SportsStore2.Controllers
     public class ProductController : Controller
     {
         private IProductRepository repository;
-
+        public int PageSize = 4;
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
 
-        public ViewResult List()
+        public ViewResult List(int productPage=1)
         {
-            return View(repository.Products);
+            return View(repository.Products.OrderBy(p=>p.ProductID).Skip((productPage-1)*PageSize).Take(PageSize));
         }
     }
 }
