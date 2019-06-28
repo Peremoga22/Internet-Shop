fbec048b965fa503219430fc8ede1af377fde3a0
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-29 00:20:32 +0300

    Delete UnitTest1

---
+++ SportsStore2/SportsStoreTest/ProductControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using SportsStore2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace SportsStoreTest
+{
+   public class ProductControllerTests
+    {
+        [Fact]
+        public void Can_Paginate()
+        {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable<Product>());
+        }
+
+    }
+}
