dabc4fdacd0157a6fa52f3e3522a2e8323e49837
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-28 23:45:34 +0300

    Update ProductsListViewModel
    Add property

--- SportsStore2/SportsStore2/Models/ViewModels/ProductsListViewModel.cs
+++ SportsStore2/SportsStore2/Models/ViewModels/ProductsListViewModel.cs
@@ -8,6 +8,6 @@ namespace SportsStore2.Models.ViewModels
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
-
+        public PagingInfo PagingInfo { get; set; }
     }
 }
