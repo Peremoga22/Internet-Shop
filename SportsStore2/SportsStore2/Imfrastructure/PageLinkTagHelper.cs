f276078f5459efa9e42bcf17a7442c2abf381339
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-29 22:58:13 +0300

    Create PageLinkTagHelper
    This class will be auxiliary, it fills the elements <div> an element "a" which match the product pages.

---
+++ SportsStore2/SportsStore2/Imfrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore2.Imfrastructure
+{
+    public class PageLinkTagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+    }
+}
