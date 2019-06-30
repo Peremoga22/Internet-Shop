c48d214e7859ccb54705b8b504378fd401582358
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-06-30 14:08:38 +0300

    Create Cart.cs
    Add List;
    add method  who choeses product for cart;

---
+++ SportsStore2/SportsStore2/Models/Cart.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore2.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public virtual void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(p => p.Product.ProductID == product.ProductID)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+    }
+}
