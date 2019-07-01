2b008a5e7b7f8e7b295043dec48e27148d4f8d1b
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-07-01 12:53:26 +0300

    Update Order.cs
    Add atributes;

--- SportsStore2/SportsStore2/Models/Orders.cs
+++ SportsStore2/SportsStore2/Models/Orders.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,22 +9,25 @@ namespace SportsStore2.Models
 {
     public class Orders
     {
+        [BindNever]
         public int OrderId { get; set; }
-
+        [BindNever]
         public ICollection<CartLine> Lines { get; set; }
-
+        [Required(ErrorMessage ="Please entry name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter  the first address line")]
         public string Street { get; set; }
 
         public string NumberHouse { get; set; }
 
         public string NumberFlat { get; set; }
-
+        [Required(ErrorMessage = "Please enter  a city name")]
         public string City { get; set; }
-
+        [Required(ErrorMessage = "Please enter  a oblast name")]
         public string Oblast { get; set; }
 
         public string Zip { get; set; }
-
+        [Required(ErrorMessage = "Please enter  a country name")]
         public string Country { get; set; }
 
         public bool GiftWrap { get; set; }
