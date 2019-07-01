2e04f8888c39192965c2870c3ddcefda624eef73
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-07-01 12:45:12 +0300

    Create Order.cs
    Add filds and property;

---
+++ SportsStore2/SportsStore2/Models/Orders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore2.Models
+{
+    public class Orders
+    {
+        public int OrderId { get; set; }
+
+        public ICollection<CartLine> Lines { get; set; }
+
+        public string Street { get; set; }
+
+        public string NumberHouse { get; set; }
+
+        public string NumberFlat { get; set; }
+
+        public string City { get; set; }
+
+        public string Oblast { get; set; }
+
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+
+        public bool GiftWrap { get; set; }
+    }
+}
