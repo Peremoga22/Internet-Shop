b7221c669eb4cb9602e0197e9f65a14cd66b50d1
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-07-01 13:31:28 +0300

    Create  IOrderRepository.cs
    Add  IQuareable-This interface allows you to work with a database;
    Add method :SaveOrder;

---
+++ SportsStore2/SportsStore2/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore2.Models;
+
+namespace SportsStore2.Controllers
+{
+    public class OrderController : Controller
+    { 
+        private Cart cart;
+
+        public ViewResult Checkout() => View(new Order());
+    }
+}
