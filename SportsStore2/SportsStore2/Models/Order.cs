362327bae05ec9db4154c44cfc7233ddba8f1913
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-07-02 01:22:24 +0300

    Update Order
     Replace the name of the fields

--- SportsStore2/SportsStore2/Models/Order.cs
+++ SportsStore2/SportsStore2/Models/Order.cs
@@ -17,16 +17,18 @@ namespace SportsStore2.Models
         public bool Shipped { get; set; }
         [Required(ErrorMessage ="Please entry name")]
         public string Name { get; set; }
-        [Required(ErrorMessage ="Please enter  the first address line")]
-        public string Street { get; set; }
 
-        public int NumberHouse { get; set; }
+        [Required(ErrorMessage = "Please enter the first address line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
 
-        public int NumberFlat { get; set; }
-        [Required(ErrorMessage = "Please enter  a city name")]
+        [Required(ErrorMessage = "Please enter a city name")]
         public string City { get; set; }
-        [Required(ErrorMessage = "Please enter  a oblast name")]
-        public string Oblast { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter a state name")]
+        public string State { get; set; }
 
         public int Zip { get; set; }
         [Required(ErrorMessage = "Please enter  a country name")]
