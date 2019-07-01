25946c4ae37ad47f7bb3011f0e6f9dce1456dc33
Vasyl <45177832+Peremoga22@users.noreply.github.com>
2019-07-02 01:50:49 +0300

    Create AdminController.cs

---
+++ SportsStore2/SportsStore2/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore2.Models;
+
+namespace SportsStore2.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+        public AdminController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(repository.Products);
+        }
+    }
+}
